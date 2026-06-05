2815238e964a18af52099cf464ac8415f8bf9208
Lennart Breede <lennart.breede@gmail.com>
2026-06-04 18:40:24 -0700

    swift-format

--- Sources/MonkeyInterpreter/main.swift
+++ Sources/MonkeyInterpreter/main.swift
@@ -6,11 +6,10 @@
 //
 
 #if canImport(Glibc)
-    import Glibc
+  import Glibc
 #elseif canImport(Darwin)
-    import Darwin
+  import Darwin
 #endif
-
 
 let user = getenv("USER").map { String(cString: $0) } ?? "stranger"
 print("Hello \(user)! This is the Monkey programming language!")
