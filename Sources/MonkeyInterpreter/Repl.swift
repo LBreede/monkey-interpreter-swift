8903c57764e429ee85dd338ad8ca75673a99d7f2
Lennart Breede <lennart.breede@gmail.com>
2026-06-12 17:55:43 -0700

    Added parser to repl

--- Sources/MonkeyInterpreter/Repl.swift
+++ Sources/MonkeyInterpreter/Repl.swift
@@ -1,10 +1,29 @@
-func startRepl() {
+enum ReplMode {
+  case tokens
+  case statements
+}
+
+func startRepl(mode: ReplMode = .statements) {
   let prompt = ">> "
   while true {
     print(prompt, terminator: "")
     guard let input = readLine() else { return }
-    for token in Lexer(input: input) {
-      print(token)
+
+    switch mode {
+    case .tokens:
+      for token in Lexer(input: input) {
+        print(token)
+      }
+    case .statements:
+      var parser = Parser(lexer: Lexer(input: input))
+      let program = parser.parseProgram()
+      guard parser.errors.isEmpty else {
+        for message in parser.errors {
+          print("\t" + message)
+        }
+        continue
+      }
+      print(program)
     }
   }
 }
--- Sources/MonkeyInterpreter/main.swift
+++ Sources/MonkeyInterpreter/main.swift
@@ -7,4 +7,5 @@
 let user = getenv("USER").map { String(cString: $0) } ?? "stranger"
 print("Hello \(user)! This is the Monkey programming language!")
 print("Feel free to type in commands")
-startRepl()
+let mode: ReplMode = CommandLine.arguments.contains("--tokens") ? .tokens : .statements
+startRepl(mode: mode)
